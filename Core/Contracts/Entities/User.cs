18e7884d1c05964a676a92d252b1344e817aadcb
manuelturlan01 <manuelturlan21@gmail.com>
2023-08-23 20:32:59 -0400

    Add packages and create User entity

---
+++ Core/Contracts/Entities/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Contracts.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            DateCreated = DateTime.UtcNow;
+        }
+        /* ==========================
+            * USER BASICS DATA
+        * ========================== */
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        /* ==========================
+            * ROLE AND PERMISSIONS
+        * ========================== */
+        public UserRole Role { get; set; }
+        public enum UserRole
+        {
+            Admin,
+            User,
+            Guest
+        }
+        /* ==========================
+            * AUTH AND SECURITY
+        * ========================== */
+        public DateTime LastLogin { get; set; }
+        public int FailedLoginAttemps { get; set; }
+        public bool IsLockedOut { get; set; }
+        public DateTime? LockoutEnd { get; set; }
+        /* ==========================
+            * OTHER
+        * ========================== */
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
